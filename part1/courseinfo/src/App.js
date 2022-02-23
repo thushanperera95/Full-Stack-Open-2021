8f45a1cdfb9b4aa63e5a4f09f594d5c5fc431644
Thushan <thushan.perera95@gmail.com>
2022-02-23 21:27:04 +0800

    Refactoring

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,64 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.name} {props.exercises}
+      </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.parts.map(element => <Part key={element.name} name={element.name} exercises={element.exercises} />)
+  return (
+    <div>
+      {parts}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App;
