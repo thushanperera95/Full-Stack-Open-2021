5d698d429906ae38c5435124ee78f4e9d2f9f1b9
Thushan <thushan.perera95@gmail.com>
2022-03-10 16:04:11 +0800

    Completed part 2-c phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
 
-  const isValidName = (name) => !persons.some(person => person.name === name)
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -24,6 +25,8 @@ const App = () => {
       name: newName,
       number: newNumber
     }
+
+    const isValidName = (name) => !persons.some(person => person.name === name)
 
     if (!isValidName(personObject.name)) {
       alert(`${personObject.name} is already added to phonebook`)
@@ -35,7 +38,7 @@ const App = () => {
     setNewNumber('')
   }
 
-  const personsToShow = filterText.length == 0 
+  const personsToShow = filterText.length == 0
     ? persons
     : persons.filter(person => person.name.toLowerCase().startsWith(filterText.toLowerCase()))
 
