df7cba9f909f3148c669bdda0d6d759851f08b58
Thushan <thushan.perera95@gmail.com>
2022-05-31 14:27:56 +0800

    Implemented patientor backend

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
+
 const app = express();
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -12,6 +15,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
